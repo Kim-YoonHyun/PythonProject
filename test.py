2786f870a6188aec955aad170782b90f0eddda92
Yoonhyun Kim <kim_yh663927@naver.com>
2021-08-11 19:35:46 +0900

    main commit

--- test.py
+++ test.py
@@ -5,48 +5,76 @@ import random
 import time
 import dill
 import math
+import pandas as pd
+
+# s = pd.Series([100, 200, 300])
+# # print(s, '\n')
 #
-# with open('input_data.pkl', 'rb') as f:
-#     input_data = dill.load(f)
-# print(input_data)
+# s = pd.Series([100, 200, 300],
+#               index=['서울', '대구', '부산'], dtype=np.int32)
+#
+# # print(s)
+# # print(type(s.values))
+# # print(type(s.index))
+# # print(s.index)
+# s.name = '인구'
+# s.index.name = '도시'
+# # print(s)
+# # print(s/10)
+# # print(s[0])
+# # print(s['서울'])
+# # print(s.서울)
+# # print(s[[0, 2]])
+# # print(s[['서울', '부산']])
+# # print(s[1:3])
+# # print(s['대구':'부산'])
+# # print(s[(100 < s) & (s < 300)])
+# # print(s[(s < 100) | (s > 200)])
+# s2 = pd.Series({'서울':300, '대구':200, '대전':100})
+# rs = s - s2
+# # print((s-s2)/s * 100)
+# # print(s.values - s2.values)
+# # print(rs.notnull())
+# # print(rs[rs.notnull()])
+# rs = rs[rs.notnull()]
+# rs['청주'] = 600
+# print(rs)
+# del rs['서울']
+# print(rs)
 
-
-def make_rotate_angle(number, x1, x2, y1, y2, z1, z2):
-    x_degree = random.uniform(x1, x2) * (math.pi / 180)
-    y_degree = random.uniform(y1, y2) * (math.pi / 180)
-    z_degree = random.uniform(z1, z2) * (math.pi / 180)
-    return x_degree, y_degree, z_degree
-
-
-xd, yd, zd = make_rotate_angle(2, -10, 10, -10, 10, -5, 5)
-
-print(xd*180/math.pi)
-print(yd*180/math.pi)
-print(zd*180/math.pi)
-
-
-def make_rotation_matrix(xyz_angle):
-    x_matrix = np.expand_dims(np.array([
-        [1.,                       0.,                      0.],
-        [0.,   math.cos(xyz_angle[0]), -math.sin(xyz_angle[0])],
-        [0.,   math.sin(xyz_angle[0]),  math.cos(xyz_angle[0])]]), axis=0)
-    y_matrix = np.expand_dims(np.array([
-        [math.cos(xyz_angle[1]),   0.,  math.sin(xyz_angle[1])],
-        [0.,                       1.,                      0.],
-        [-math.sin(xyz_angle[1]),  0.,  math.cos(xyz_angle[1])]]), axis=0)
-    z_matrix = np.expand_dims(np.array([
-        [math.cos(xyz_angle[2]), -math.sin(xyz_angle[2]),   0.],
-        [math.sin(xyz_angle[2]),  math.cos(xyz_angle[2]),   0.],
-        [0.,                       0.,                      1.]]), axis=0)
-    return np.concatenate((x_matrix, y_matrix, z_matrix), axis=0)
-
-
-x = make_rotation_matrix([xd, yd, zd])
-b = np.random.randint(-6, 6, 36).reshape(6, 2, 3)
-
-print(np.dot(b, x[0]))
-print(np.dot(b[0,0], x[0]))
-print(np.dot(b[0,1], x[0]))
-print(np.dot(b[1,0], x[0]))
-print(np.dot(b[1,1], x[0]))
-print(np.dot(b[0,0], x[0]))
+data = {
+    "지역": ["수도권", "경상권", "경상권"],
+    "2015": [100, 200, 300],
+    "2010": [400, 500, 600],
+    "2005": [700, 800, 900],
+    "2000": [1000, 1100, 1200],
+    "2010-2015 증가율": [0.01, 0.02, 0.03]
+}
+columns = ["지역", "2000", "2005", "2010", "2015", "2010-2015 증가율"]
+index = ["서울", "부산", "대구"]
+df = pd.DataFrame(data, index=index, columns=columns)
+# print(df)
+# print(df.지역.values)
+# print(df['2000'])
+# print(df.columns)
+# print(df.columns.values)
+df.index.name = '도시'
+df.columns.name = '특성'
+# print(df.T)
+df['2010-2015 증가율'] = df['2010-2015 증가율'] * 100
+# print(df)
+df['2020'] = [1, 2, 3]
+# print(df
+del df['2020']
+# print(df)
+# print(df['지역'])
+# print(df[['지역']])
+# print(df[['지역', '2015']])
+# print(df['서울':'부산'])
+# print(df[1:3])
+# print(df.head(2))
+# print(df.head())
+# print(df['2015']['서울'])
+# print(df['서울']['2015'])
+df['2015']['서울'] = 100
+print(df.values)
